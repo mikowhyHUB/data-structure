54dde73fdafc363222f3a9dac23d2ae5a1ececde
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-16 18:47:58 +0100

    new exercise

---
+++ ex17.py
@@ -0,0 +1,9 @@
+'''Write a Python script to check whether a given key already exists in a dictionary'''
+
+d = {1: 10, 2: 20, 3: 30}
+x = 5
+
+if x in d:
+    print("exists")
+else:
+    print('dont exists')
