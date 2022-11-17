11def9fef3aa6281c2bd8bd9e311cbbaf5605227
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2022-11-17 16:24:24 +0100

    new exercises

---
+++ ex23.py
@@ -0,0 +1,9 @@
+'''Write a Python program to get the maximum and minimum value in a dictionary.'''
+
+d1 = {'a': 100, 'b': 200, 'c': 300}
+
+maximum = max(d1.items())
+miniumum = min(d1.values())
+
+print(maximum)
+print(miniumum)
